5690cc7bf2445b3293e4fbd01b010b336261e267
amplee <amplee@sohu-inc.com>
2018-10-03 23:30:09 +0800

    test

--- app/src/main/java/com/wizard/pdfreader/MainActivity.java
+++ app/src/main/java/com/wizard/pdfreader/MainActivity.java
@@ -12,6 +12,6 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        Log.i("wizard","test");
+        Log.i("wizard","testt");
     }
 }
