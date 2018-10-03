9500394411fd6704be6cb4309e6abc6b215d926d
amplee <amplee@sohu-inc.com>
2018-10-03 23:06:22 +0800

    首次提交

---
+++ app/src/main/java/com/wizard/pdfreader/MainActivity.java
@@ -0,0 +1,15 @@
+package com.wizard.pdfreader;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import org.androidannotations.annotations.EActivity;
+
+@EActivity(R.layout.activity_main)
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
