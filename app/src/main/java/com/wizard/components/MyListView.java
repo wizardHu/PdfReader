a44b6baec894ca51d5dc4ad6cadd1e2b9b1206fd
amplee <amplee@sohu-inc.com>
2018-10-04 23:15:00 +0800

    沉浸式1.0

---
+++ app/src/main/java/com/wizard/components/MyListView.java
@@ -0,0 +1,34 @@
+package com.wizard.components;
+
+import android.content.Context;
+import android.os.Build;
+import android.support.v4.view.NestedScrollingChild;
+import android.support.v4.view.NestedScrollingChildHelper;
+import android.util.AttributeSet;
+import android.widget.ListView;
+
+public class MyListView extends ListView{
+
+    public MyListView(Context context) {
+        super(context);
+    }
+
+    public MyListView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public MyListView(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+
+
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        //测量的大小由一个32位的数字表示，前两位表示测量模式，后30位表示大小，这里需要右移两位才能拿到测量的大小
+        int heightSpec = MeasureSpec.makeMeasureSpec(Integer.MAX_VALUE >> 2, MeasureSpec.AT_MOST);
+        super.onMeasure(widthMeasureSpec, heightSpec);
+    }
+
+
+}
--- app/src/main/java/com/wizard/pdfreader/MainActivity.java
+++ app/src/main/java/com/wizard/pdfreader/MainActivity.java
@@ -1,17 +1,46 @@
 package com.wizard.pdfreader;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.design.widget.CollapsingToolbarLayout;
+import android.support.design.widget.CoordinatorLayout;
+import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.ListView;
 
+import org.androidannotations.annotations.AfterViews;
 import org.androidannotations.annotations.EActivity;
+import org.androidannotations.annotations.ViewById;
 
 @EActivity(R.layout.activity_main)
 public class MainActivity extends AppCompatActivity {
+
+    private String[] data = { "Apple", "Banana", "Orange", "Watermelon",
+            "Pear", "Grape", "Pineapple", "Strawberry", "Cherry", "Mango", "Mango", "Mango", "Mango", "Mango", "Mango" };
+
+    @ViewById
+    ListView listview;
+
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         Log.i("wizard","test");
+
+
+
+
+
+    }
+
+    @AfterViews
+    void afterViews() {
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>( MainActivity.this, android.R.layout.simple_list_item_1, data);
+        listview.setAdapter(adapter);
     }
 }
